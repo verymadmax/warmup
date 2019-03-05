2822b509c47c5253d30264f88f25d7d2e0a70f90
verymadmax <madmax.verymad@gmail.com>
2019-03-06 01:34:21 +0300

    feat: add function which celcius to fahrenheit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 module.exports = function warmup(temperature) {
-  // current temperature in kelvin degrees
-  var fahrenheit;
-  // convering kelvin to celsius
-  let kelvin = celsius + 273;
-  // convering celsius to fahrenheit
-  let celsius = fahrenheit -32 * (5/9);
-  //round down fahrenheit variable
-  fahrenheit = Math.floor(fahrenheit);
-  console.log(`The temp ${fahrenheit} degrees Fahrenheit`)
-};
+  return 9/5*temperature + 32;
+ };
