be62089e16a877640799036de19f7603cd794b7f
verymadmax <madmax.verymad@gmail.com>
2019-03-05 17:32:38 +0300

    begin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  // current temperature in kelvin degrees
+  var fahrenheit;
+  // convering kelvin to celsius
+  let kelvin = celsius + 273;
+  // convering celsius to fahrenheit
+  let celsius = fahrenheit -32 * (5/9);
+  //round down fahrenheit variable
+  fahrenheit = Math.floor(fahrenheit);
+  console.log(`The temp ${fahrenheit} degrees Fahrenheit`)
 };
